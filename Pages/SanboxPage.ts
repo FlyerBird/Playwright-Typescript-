a7fe02db44e1fa3c83ee53b75d39561884bb0097
Carlos Nieto <99897631+FlyerBird@users.noreply.github.com>
2024-03-10 19:43:31 +0100

    Add files via upload

---
+++ Pages/SanboxPage.ts
@@ -0,0 +1,14 @@
+import { type Locator, type Page } from "@playwright/test";
+
+
+export class SandboxPage {
+    readonly page:Page;
+    readonly pastaCheckbox: Locator;
+
+    constructor(page:Page){
+        this.page = page;
+        this.pastaCheckbox = page.getByLabel('Pasta 🍝');
+    }
+
+}
+
