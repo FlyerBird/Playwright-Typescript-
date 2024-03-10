955d4c89f609ee161281536ac378d7b5e1a6ba23
Carlos Nieto <99897631+FlyerBird@users.noreply.github.com>
2024-03-10 20:12:03 +0100

    Add files via upload

--- Pages/SanboxPage.ts
+++ Pages/SanboxPage.ts
@@ -10,5 +10,11 @@ export class SandboxPage {
         this.pastaCheckbox = page.getByLabel('Pasta 🍝');
     }
 
+    async checkPasta() {
+        await this.pastaCheckbox.check();
+    }
+
 }
 
+
+// modelando un página solamente tengo elementos web (Locators) y acciones para esos elementos. No validaciones (assertions).
